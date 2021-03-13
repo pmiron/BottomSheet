5db7f5aecaf173769d897ded5f6522a609e4b66d
Pavel Miron <pavelmir@ext.zara.com>
2021-03-13 23:32:34 +0100

    Added basic structure

---
+++ app/src/main/java/md/holatech/boottomsheet/CustomBottomSheetDialogFragment.kt
@@ -0,0 +1,40 @@
+package md.holatech.boottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.layout_modal_bottom_sheet.*
+
+class CustomBottomSheetDialogFragment : BottomSheetDialogFragment(){
+    companion object {
+        const val TAG = "CustomBottomSheetDialogFragment"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.layout_modal_bottom_sheet, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        firstButton.setOnClickListener {
+            //handle click event
+            Toast.makeText(context, "First Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+        secondButton.setOnClickListener {
+            //handle click event
+            Toast.makeText(context, "Second Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+        thirdButton.setOnClickListener {
+            //handle click event
+            Toast.makeText(context, "Third Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/md/holatech/boottomsheet/MainActivity.kt
+++ app/src/main/java/md/holatech/boottomsheet/MainActivity.kt
@@ -2,10 +2,16 @@ package md.holatech.boottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firstButton.setOnClickListener {
+            val mBottomSheetFragment = ModalBottomSheet()
+            mBottomSheetFragment.show(supportFragmentManager, "MY_BOTTOM_SHEET")
+        }
     }
 }
