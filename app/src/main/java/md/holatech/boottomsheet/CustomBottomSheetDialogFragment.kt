a5b2cb2175c919f2cef84434d9cbd25602777d4c
Pavel Miron <pavelmir@ext.zara.com>
2021-03-14 17:09:02 +0100

    Added dialog on state change

--- app/src/main/java/md/holatech/boottomsheet/CustomBottomSheetDialogFragment.kt
+++ app/src/main/java/md/holatech/boottomsheet/CustomBottomSheetDialogFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.fragment_custom_bottom_sheet_dialog.*
 
@@ -23,6 +25,33 @@ class CustomBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        (dialog as BottomSheetDialog).behavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        Toast.makeText(context, "STATE_COLLAPSED", Toast.LENGTH_SHORT).show()
+                    }
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                        Toast.makeText(context, "STATE_HIDDEN", Toast.LENGTH_SHORT).show()
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        Toast.makeText(context, "STATE_EXPANDED", Toast.LENGTH_SHORT).show()
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                        Toast.makeText(context, "STATE_DRAGGING", Toast.LENGTH_SHORT).show()
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                        Toast.makeText(context, "STATE_SETTLING", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Toast.makeText(context, "On Slide", Toast.LENGTH_SHORT).show()
+            }
+        })
 
         firstContentButton.setOnClickListener {
             Toast.makeText(context, "First Button Clicked", Toast.LENGTH_SHORT).show()
